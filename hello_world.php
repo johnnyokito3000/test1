515878c3234879ceeacf973c96aecb178f28811a
jeano <jeano@sekuremerchants.com>
2017-04-17 09:45:19 -0400

    correction on fifth line

--- hello_world.php
+++ hello_world.php
@@ -3,5 +3,6 @@
 echo "Hello world!<br/>";
 echo "Adding a second line<br/>";
 echo "Now adding a third line<br/>";
-echo "Adding a fourth line<br/>";"
+echo "Adding a fourth line<br/>";
+
 echo "Adding a fifth line<br/>";
